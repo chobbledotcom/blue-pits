c55de47c6c886661f4ed14173accf6304ef5a544
Stefan <git@stefn.co.uk>
2023-08-22 02:34:02 +0100

    nearly

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ JEKYLL_ENV=production bundle exec jekyll build
 html-minifier --input-dir _site --output-dir _site --file-ext html --collapse-whitespace --minify-css --remove-comments --remove-attribute-quotes --remove-redundant-attributes
 
 set +x
-export NEOCITIES_API_KEY=$(cat /home/user/.neocities/blue-pits)
+export NEOCITIES_API_KEY=$(cat /home/$0/.neocities/blue-pits)
 set -x
 
 gem install --no-document neocities
