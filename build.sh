a9f8c323f1366ee8545f05f13d67d72fa1218a27
Stefan <git@stefn.co.uk>
2024-11-06 23:47:27 +0000

    Use nix-shell for everything

--- build.sh
+++ build.sh
@@ -1,11 +1,26 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 
-export JEKYLL_ENV=production
-set +x
-export NEOCITIES_API_KEY=$(cat ~/.neocities/blue-pits)
-set -x
 rm -rf _site
-bundle install
-bundle exec jekyll build
-html-minifier --input-dir _site --output-dir _site --collapse-whitespace --file-ext html
-neocities push --prune _site
+
+nix-shell -p bundler --pure --command "
+  export JEKYLL_ENV=production
+  echo 'Installing bundles'
+  bundle install --gemfile=Gemfile --path vendor/cache
+
+  echo 'Building site'
+  bundle exec jekyll build
+"
+
+nix-shell -p html-minifier --pure --command "
+  echo 'Minifying site'
+  html-minifier --input-dir _site --output-dir _site --collapse-whitespace --file-ext html
+"
+
+nix-shell -p neocities-cli --command "
+  echo 'Pushing to Neocities'
+
+  set +x
+  export NEOCITIES_API_KEY=$(cat ~/.neocities/blue-pits)
+  set -x
+  neocities push --prune _site
+"
